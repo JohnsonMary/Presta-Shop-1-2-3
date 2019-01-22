b6197c6d989f53b9b630e837cb9c20b7b5916238
JohnsonMary <45155816+JohnsonMary@users.noreply.github.com>
2019-01-22 11:55:09 -0500

    Update AccountInformation.java

--- src/test/java/com/prestashop/tests/smoke_tests/AccountInformation.java
+++ src/test/java/com/prestashop/tests/smoke_tests/AccountInformation.java
@@ -23,12 +23,7 @@ public class AccountInformation extends TestBase {
 
     @Test
     public void myAccount() throws InterruptedException {
-/*
-1. Go to http://automationpractice.com/index.php
-2. Click Sign in link
-3. Login using valid username and password
-4. Verify that title contains My account
- */
+
         page1.open();
         page1.signIn.click();
 
@@ -38,26 +33,13 @@ public class AccountInformation extends TestBase {
         Thread.sleep(1000);
 
         Assert.assertTrue(Driver.getDriver().getTitle().contains("My account"));
-/*
-5. Verify that account holder full name is displayed next to the Sign out link
-Login: My personal information
-6. Click on My personal information button
-7. Verify title contains Identity
-
- */
-
 
         Assert.assertEquals(page1.fullName(ConfigurationReader.getProperty("fullName")).getText(),ConfigurationReader.getProperty("fullName"));
         page1.personalInfButton.click();
 
         Assert.assertTrue(Driver.getDriver().getTitle().contains("Identity"));
         Thread.sleep(1000);
-/*
-8. Verify that first name and last name matches the full name on top
-9. Click on Save button
-10. Verify error message “The password you entered is incorrect.”
 
- */
         String firstName = page1.firstName.getAttribute("value");
         String lastName = page1.lastName.getAttribute("value");
 
@@ -67,30 +49,18 @@ Login: My personal information
         Assert.assertEquals(page1.errorMessage.getText(),"The password you entered is incorrect.");
 
         Thread.sleep(1000);
-/*
-11. Click on Back to your account
-12. Verify that title contains My account
-Login: My addresses
-13. Click on My addresses
-14. Click on Add a new address
-15. Verify that first name and last name matches the full name on top
-16. Delete the first name
-17. Click save
-18. Verify error message “firstname is required.”
 
+        
+        page1.backToAccountButton.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("My account"));
 
+        page1.myAddress.click();
+        page1.addNewAddress.click();
+        Assert.assertEquals(page1.fullNameOnTop.getText(),ConfigurationReader.getProperty("fullName"));
+        page1.firstname2.clear();
+        page1.saveButton2.click();
 
- */
-    page1.backToAccountButton.click();
-    Assert.assertTrue(Driver.getDriver().getTitle().contains("My account"));
-
-    page1.myAddress.click();
-    page1.addNewAddress.click();
-    Assert.assertEquals(page1.fullNameOnTop.getText(),ConfigurationReader.getProperty("fullName"));
-    page1.firstname2.clear();
-    page1.saveButton2.click();
-
-    Assert.assertTrue(page1.error.getText().contains("firstname is required"));
+        Assert.assertTrue(page1.error.getText().contains("firstname is required"));
 
     }
 
